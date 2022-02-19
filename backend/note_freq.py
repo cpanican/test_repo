21e5389b08d7cc20a866039625cbe10bbd2c0215
Islam <54121657+izy1984@users.noreply.github.com>
2022-02-18 22:22:25 -0500

    Update note_freq.py

--- backend/note_freq.py
+++ backend/note_freq.py
@@ -1,18 +1,18 @@
 import numpy as np
 from scipy.fft import *
 from scipy.io import wavfile
-from math import log2, pow
+from statistics import mean
+from math import log2, log10, pow, sqrt
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
-file = r"C:/Users/Islam/Desktop/untitled.wav"
+file = r"C:/Users/Islam/Desktop/untitled2.wav"
 
 A4 = 440
 C0 = A4*pow(2, -4.75)
 name = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
-def freq(file, start_time, end_time):
-
+def freq(file):
     # Converts file to mono
     sample_rate, data = wavfile.read(file)
     # If number of array dimensions > 1 
@@ -20,16 +20,23 @@ def freq(file, start_time, end_time):
         data = data[:, 0]
     else:
         pass
-    
-    note_list = []
+
+    end_time = ((len(data) - 1)*1000)/sample_rate
+    start_time = 0
+    freq_list = []
+    notes = []
 
     notes_num = 5
-    interval = 500
-    curr = start_time + interval
-    
-    # maximum length of .wav file in miliseconds
-    max = ((len(data) - 1)*1000)/sample_rate
+    # interval = (start_time + end_time) / notes_num
+    interval = 50
+    curr = start_time + interval    
 
+    chunks = np.array_split(data, end_time/curr)
+    # dbs = [20*log10( (mean(chunk**2))**.5 ) for chunk in chunks]
+    vol = []
+    for chunk in chunks:
+        vol.append(mean([abs(c) for c in chunk]))
+    
     while curr <= end_time:
         # Return a slice of the data from start_time to end_time
         dataToRead = data[int(start_time * sample_rate / 1000) : int(curr * sample_rate / 1000) + 1]
@@ -42,27 +49,37 @@ def freq(file, start_time, end_time):
         # Get the most dominant frequency and return it
         idx = np.argmax(np.abs(yf))
         freq = xf[idx]
-        
-        # Find note based on frequency
-        h = round(12*log2(freq/C0))
-        octave = h // 12
-        n = h % 12
-        note = name[n] + str(octave)
 
-        # If the note is already the last value in note_list, do not append
-        if len(note_list) > 0:
-            if note != note_list[-1]:
-                note_list.append(name[n] + str(octave))
-        else:
-            note_list.append(name[n] + str(octave))
-        start_time = curr
+        freq_list.append(freq)
 
         curr += interval
 
-    print(f'max = {max}')
-    return note_list
+    counter = 0
+    while counter < len(vol):
+        frequencies = []
+        while vol[counter] > 1000:
+            frequencies.append(freq_list[counter])
+            counter += 1
+        counter += 1
+        if len(frequencies) > 0:
+            notes.append(frequencies)
+    
+    return notes
+
+def note(freq):
+    # Find note based on frequency
+    h = round(12*log2(freq/C0))
+    octave = h // 12
+    n = h % 12
+    note = name[n] + str(octave)
+    return note
+
+note_list = []
+
+for i in freq(file):
+    note_list.append(note(mean(i)))
+
+print(note_list)
 
 app = Flask(__name__)
 api = Api(app)
-
-# print(freq(file, 0, 3000))
