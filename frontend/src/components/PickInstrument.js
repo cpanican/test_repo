e6491fc32942b91ab73805c94bc3afa8caae5956
Chris Panican <chrispanican@gmail.com>
2022-02-20 06:34:25 -0500

    fetch request

--- frontend/src/components/PickInstrument.js
+++ frontend/src/components/PickInstrument.js
@@ -3,39 +3,78 @@ import '../styles/PickInstrument.css';
 import { MyContext } from '../MyProvider';
 
 class PickInstrument extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      audioData: null,
+      pickedInstrument: null,
+      isLoading: false
+    }
+  }
+
+  componentDidMount() {
+    let value = this.context;
+    this.setState({
+      audioData: value.audioFile
+    })
+  }
+
+  uploadFile = (file, instrument) => {
+    console.log(this.state.audioData.blob);
+    this.setState({
+      isLoading: true
+    })
+    var formData = new FormData();
+    formData.append('file', file);
+    formData.append('instrument', instrument);
+    fetch('http://192.168.1.244:420/convert', {
+      mode: 'no-cors',
+      method: 'POST',
+      body: formData,
+    }).then(response => response.json())
+      .then(success => {
+        console.log(success)
+      }).catch(error => console.log(error))
+  }
+
   render() {
     return (
       <main className="main">
-        <h1 className="title">Pick your instrument.</h1>
-        <p className="description">To convert the music notes, select the instrument(s) below.</p>
+        {this.state.isLoading
+          ?
+          <div>
+            <h1 className="title">Converting to instrument...</h1>
+            <p className="description">Tuudle is converting your music notes to intrumental audio.</p>
 
-        <div className="buttonGrid">
-          <div>
-            <a href="/get-started">
-              <button class="btn btn-outline-secondary btn-lg tuudle-learn-btn"><i class="bi bi-arrow-left-short"></i> Record Again</button>
-            </a>
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+            </div>
           </div>
-          <div></div>
+          :
           <div>
-            <a href="/get-started">
-              <button class="btn btn-primary btn-lg tuudle-purple-btn">Convert</button>
-            </a>
-            
+            <h1 className="title">Pick your instrument.</h1>
+            <p className="description">To convert the music notes, select the instrument(s) below.</p>
+
+            <div className="buttonGrid">
+              <div>
+                <a href="/get-started">
+                  <button class="btn btn-outline-secondary btn-lg tuudle-learn-btn"><i class="bi bi-arrow-left-short"></i> Record Again</button>
+                </a>
+              </div>
+              <div></div>
+              <div>
+                <button class="btn btn-primary btn-lg tuudle-purple-btn" onClick={() => {
+                  this.uploadFile(this.state.audioData.blob, this.state.pickedInstrument);
+                }}>Convert</button>
+              </div>
+            </div>
           </div>
-          <MyContext.Consumer>
-            {({audioFile, setAudioFile, audioFileTester}) => (
-              <button
-              className='btn btn-primary btn-lg tuudle-purple-btn continue-btn'
-              onClick={audioFileTester}
-              >
-                test
-              </button>
-            )}
-          </MyContext.Consumer>
-        </div>
+        }
+        
       </main>
     );
   }
 }
-
+PickInstrument.contextType = MyContext;
 export default PickInstrument;
