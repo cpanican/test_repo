7944c1659c945bf56c7e39373fa6407c3a68403b
Chris Panican <chrispanican@gmail.com>
2022-02-19 20:39:51 -0500

    merge conflict"

--- frontend/pages/get-started/index.js
+++ frontend/pages/get-started/index.js
@@ -1,20 +1,11 @@
 import styles from '../../styles/Home.module.css'
-<<<<<<< HEAD
-
 export default function GetStarted() {
     return (
-        <div>
-            <h1 className={styles.title}>Sweet! Let's get started. </h1>
-            <p className={styles.description}>Click on the record button and start whistling a tune. Once you're done, click finish recording and let Tuudle do its magic!</p>
-=======
-export default function GetStarted() {
-    return (
-        <div>
+    <div>
       <main className={styles.main}>
         <h1 className={styles.title}>
           Sweet! Let's Get Started.
         </h1>
->>>>>>> 81f3a9783d8c6494f5252ba9d0c52e239ae47812
 
         <p className={styles.description}>
           Click on the record button and start whistling a tune. Once you're done, click finish recording and let Tuudle do its magic!
