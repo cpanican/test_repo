3941de75ef24162b518aad8aea619f4ea1966901
KHOA NGUYEN <khoa.nguyen21@myhunter.cuny.edu>
2022-02-20 02:38:19 -0500

    success page layout

--- frontend/src/components/Success.js
+++ frontend/src/components/Success.js
@@ -2,10 +2,39 @@ import React, { Component } from 'react';
 import '../styles/Success.css';
 
 class Success extends Component {
+
   render() {
     return (
       <main className="main">
         <h1>Success page</h1>
+        <h1>Your music notes are here!</h1>
+        <p className="description">
+         <p> Tuudle completed the instrumental conversion. </p>
+         <p> Download your tunes below. </p>
+         <p> Keep rockin'! </p>
+        </p>
+
+
+       <div>
+
+        <button>
+        <img source="pianoframe" src="/images/redpiano.jpg" width ="200" height = "200" ></img>
+        </button>
+
+
+
+        <button>
+        <img source="pianoframe" src="/images/guitar.jpg" width ="200" height = "200"></img>
+        </button>
+
+
+
+        <button>
+        <img source="pianoframe" src="/images/electric-guitar.jpg" width ="200" height = "200"></img>
+        </button>
+        </div>
+
+
       </main>
     );
   }
