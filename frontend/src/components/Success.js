07f14949edef44a05f6f01f9e2a37ab73fd8e6d2
Chris Panican <chrispanican@gmail.com>
2022-02-20 00:27:22 -0500

    create all pages

---
+++ frontend/src/components/Success.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/Success.css';
+
+class Success extends Component {
+  render() {
+    return (
+      <main className="main">
+        <h1>Success page</h1>
+      </main>
+    );
+  }
+}
+
+export default Success;
